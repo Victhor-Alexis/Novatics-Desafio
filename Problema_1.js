1ff38f3569954efdc6eb49270168b563f9cd4dec
Victhor-Alexis <victhoralexis123@gmail.com>
2022-02-05 18:44:38 -0300

    :bug: Correçao no algoritmo do problema_1
    Agora o algoritmo elimina remove todos os elementos repetidos para um mesmo numero, nao somente um duplicado, mas triplicado, quadruplicado, como podemos ver no ultimo console.log de teste.

--- Problema_1.js
+++ Problema_1.js
@@ -3,8 +3,11 @@ const SortAndRemove = (arr) => {
 
     for (let i = 0; i < arr.length; ++i) { // removendo elementos duplicados
         if (i > 0 && arr[i] == arr[i-1]) {
-            arr.splice(i, 1);
-        }
+            arr.splice(i, 1)
+            --i 
+            /* antes, o algoritmo estava 'pulando' a posição, o que impedia de
+               de eliminar mais de dois elementos repetidos */  
+	    }
     }
 
     return arr;
@@ -15,3 +18,4 @@ console.log(SortAndRemove([8, 5, 10, 5, 2, 4, 4, 3]));
 console.log(SortAndRemove([6, 10, 9, 31, 42, 53]))
 console.log(SortAndRemove([10, 10, 9, 8, 7, 9]))
 console.log(SortAndRemove([200, -300, -9, 13, -88, 7, -88]))
+console.log(SortAndRemove([12, 7, 7, 7, 10, 10, 10, 10, 10, 9, 9, 9, 9]))
